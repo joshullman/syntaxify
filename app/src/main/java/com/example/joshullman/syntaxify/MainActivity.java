4f6260281addd06e6ed930a41e6c0a07635ed050
Josh Ullman <joshullman@gmail.com>
2016-10-18 14:09:04 -0700

    add speech-to-text function

--- app/src/main/java/com/example/joshullman/syntaxify/MainActivity.java
+++ app/src/main/java/com/example/joshullman/syntaxify/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.joshullman.syntaxify;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTEXT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTEXT = (TextView)findViewById(R.id.TVresult);
+    }
+
+    public void onButtonClick(View view) {
+        if(view.getId() == R.id.imageButton) {
+            promptSpeechInput();
+        }
+    }
+
+    public void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something!");
+
+        try {
+            startActivityForResult(intent, 100);
+        }
+        catch(ActivityNotFoundException a) {
+            Toast.makeText(MainActivity.this, "Sorry, your device doesn't support speech language!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onActivityResult(int request_code, int result_code, Intent intent) {
+        super.onActivityResult(request_code, result_code, intent);
+
+        switch(request_code) {
+            case 100: if(result_code == RESULT_OK && intent != null) {
+                ArrayList<String> result = intent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                resultTEXT.setText(result.get(0));
+            }
+                break;
+        }
     }
 }
